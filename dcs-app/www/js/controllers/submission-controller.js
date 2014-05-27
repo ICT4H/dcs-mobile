50861070fa24977a647b6b636bcad82f5501de17
princemjain <princemj@thoughtworks.com>
2014-05-27 11:02:51 +0530

    Prince | Enchanced UI: setting appropriate button label in enketo form.

--- dcs-app/www/js/controllers/submission-controller.js
+++ dcs-app/www/js/controllers/submission-controller.js
@@ -1,7 +1,7 @@
 dcsApp.controller('submissionController', ['$rootScope', '$routeParams', 'dbService', function($rootScope, $routeParams, dbService){
     
     var surveyResponseId = $routeParams.surveyResponseId;
-
+    var buttonLabel = 'Save';
     var onSuccess = function(message){
     	$rootScope.displaySuccess(message);
     };
@@ -9,6 +9,17 @@ dcsApp.controller('submissionController', ['$rootScope', '$routeParams', 'dbServ
     	$rootScope.displayError(message);
     };	
 
-    loadEnketo(dbService, $routeParams.surveyId, surveyResponseId, onSuccess, onError);
+    if(surveyResponseId != "null")
+    	buttonLabel = 'Update';
+
+    options = {
+    	'dbService' : dbService,
+    	'buttonLabel' : buttonLabel,
+		'surveyId' : $routeParams.surveyId,
+		'surveyResponseId' : surveyResponseId,
+		'onSuccess' : onSuccess,
+		'onError' : onError
+	};
+    loadEnketo(options);
 
 }]);
