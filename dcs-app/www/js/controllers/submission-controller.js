17b51e5da21ccd7ba14518db52f274172673392f
surajbab <https://github.com/surajbabar>
2014-06-19 16:12:12 +0530

    Suraj || removed extra lines of code.

--- dcs-app/www/js/controllers/submission-controller.js
+++ dcs-app/www/js/controllers/submission-controller.js
@@ -1,14 +1,8 @@
-dcsApp.controller('submissionController', ['$rootScope', '$routeParams', 'localStore', 'messageService', function($rootScope, $routeParams, localStore, msg){
+dcsApp.controller('submissionController', ['$routeParams', 'localStore', 'messageService', function($routeParams, localStore, msg){
     
     var submission_id = $routeParams.submission_id;
     var project_id = $routeParams.project_id;
     var buttonLabel = 'Save';
-    var onSuccess = function(message){
-    	msg.displaySuccess(message);
-    };
-    var onError = function(message){
-    	msg.displayError(message);
-    };	
 
     if(submission_id != "null")
     	buttonLabel = 'Update';
@@ -18,8 +12,8 @@ dcsApp.controller('submissionController', ['$rootScope', '$routeParams', 'localS
     	'buttonLabel': buttonLabel,
 		'project_id': project_id,
 		'submission_id': submission_id,
-		'onSuccess': onSuccess,
-		'onError': onError
+		'onSuccess': msg.displaySuccess,
+		'onError': msg.displayError
 	};
     loadEnketo(options);
 
