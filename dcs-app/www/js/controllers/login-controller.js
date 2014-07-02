4bc7da2a001db8382d78c39ebf635c50e6cb93d0
Yogesh Sreedharan <yogeshsr@thoughtworks.com>
2014-07-02 15:00:10 +0530

    Yogesh || Hardcoded username and serverurl default value.

--- dcs-app/www/js/controllers/login-controller.js
+++ dcs-app/www/js/controllers/login-controller.js
@@ -3,6 +3,8 @@ dcsApp.controller('loginController', ['$rootScope', '$scope', '$location', 'user
     msg.showLoading();
     $scope.user = {};
     $scope.user.loginType = 'existing';
+    $scope.user.name = 'tester150411@gmail.com';
+    $scope.user.serverUrl = 'https://172.18.29.3';
 
     $rootScope.projects = []; // clear cache when user comes to login page
 
@@ -10,8 +12,8 @@ dcsApp.controller('loginController', ['$rootScope', '$scope', '$location', 'user
         // TODO User will be selcting existing/new check box to selct/enter user name
         // For now assuming there will be only one user per app.
         if (details.length >= 1) {
-            $scope.user.name = details[0].user_name;
-            $scope.user.serverUrl = details[0].url;
+            $scope.user.name = details[0].user_name || $scope.user.name;
+            $scope.user.serverUrl = details[0].url || $scope.user.serverUrl;
         }
         msg.hideAll();
     },function(error) {
