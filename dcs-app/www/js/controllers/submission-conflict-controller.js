d103a2dcd84b8c61a47f24bd24b7d1b6ec28dccf
Yogesh Sreedharan <yogeshsr@thoughtworks.com>
2014-07-28 18:20:14 +0530

    Yogesh || Submission conflit resolution page to support repeat and skip

--- dcs-app/www/js/controllers/submission-conflict-controller.js
+++ dcs-app/www/js/controllers/submission-conflict-controller.js
@@ -6,7 +6,6 @@ dcsApp.controller('submissionConflictController', ['$rootScope', '$scope', '$q',
     $scope.selectLocal = false;
 
     $scope.takeLocal = function(row) {
-        //$scope.resolvedSubmission
         console.log('take local value');
         $scope.selectLocal = true;
     }
@@ -14,6 +13,10 @@ dcsApp.controller('submissionConflictController', ['$rootScope', '$scope', '$q',
     $scope.takeServer = function(row) {
         console.log('take server value');
         $scope.selectLocal = false;
+    }
+
+    $scope.isArray = function(value) {
+        return (value instanceof Array);
     }
 
     $scope.save = function() {
@@ -36,16 +39,59 @@ dcsApp.controller('submissionConflictController', ['$rootScope', '$scope', '$q',
             delete $scope.headers.ds_name;
             delete $scope.headers.date;
 
-		    localStore.getSubmissionById($routeParams.submission_id)
-		    	.then(function(localSubmission) {
-				    
-				    $scope.localSubmission = localSubmission;
-		    		dcsService.getSubmissionById(project.project_uuid, localSubmission.submission_uuid)
-		    			.then(function(serverSubmission) {
-                        	$scope.serverSubmission	= serverSubmission;    				
-		    				$scope.serverSubmissionData = JSON.parse(serverSubmission.data);
-		    				msg.hideAll();
-		    			});
-		    	});
+          localStore.getSubmissionById($routeParams.submission_id)
+             .then(function(localSubmission) {
+
+                $scope.localSubmission = localSubmission;
+                delete $scope.localSubmission.data["meta"];
+                dcsService.getSubmissionById(project.project_uuid, localSubmission.submission_uuid)
+                   .then(function(serverSubmission) {
+                        $scope.serverSubmission   = serverSubmission;
+                        $scope.serverSubmissionData = JSON.parse(serverSubmission.data);
+                        delete $scope.serverSubmissionData["meta"];
+
+                        fillPaddingData($scope.localSubmission.data, $scope.serverSubmissionData);
+                        msg.hideAll();
+                   });
+             });
     });
+
+    var fillRepeat = function(repeatObjects, n) {
+        var keys = Object.keys(repeatObjects[0]);
+        for (var i=0; i<n; i++) {
+            var d = {};
+            for (var j=0; j<keys.length; j++) {
+                d[keys[j]] = '-';
+            }
+            repeatObjects.push(d);
+        }
+    }
+
+    var fillPaddingData = function(local, server) {
+
+        for (key in server) {
+            //TODO check this is available in mobile
+            if (server[key] instanceof Array) {
+                var serverRepeatCount = server[key].length;
+                var localRepeatCount = local[key].length;
+                if (serverRepeatCount == undefined || localRepeatCount == undefined)
+                    continue;
+
+                if (serverRepeatCount > localRepeatCount) {
+                    fillRepeat(local[key], serverRepeatCount-localRepeatCount);
+                } else {
+                    fillRepeat(server[key], localRepeatCount-serverRepeatCount);
+                }
+            }
+            //TODO skip inside repeats also needs to be handled for null. Try to fix this at server
+            if (local[key] === null) {
+                local[key] = '-';
+            }
+            if (server[key] === null) {
+                server[key] = '-';
+            }
+        }
+
+    }
+
 }]);
