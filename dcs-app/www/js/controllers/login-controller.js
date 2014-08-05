7a3b34e3475ba1ef6ed7db1346c615e728d92851
surajbab <surajbab@keshavan.corporate.thoughtworks.com>
2014-08-05 13:54:50 +0530

    Suraj ||Shows last visited user as default selected at login.

--- dcs-app/www/js/controllers/login-controller.js
+++ dcs-app/www/js/controllers/login-controller.js
@@ -2,13 +2,13 @@ dcsApp.controller('loginController', ['$rootScope', '$scope', '$location', 'user
     msg.showLoading();
     $scope.users = [{'name':'New', 'password': '', 'serverUrl':'https://172.18.29.3'}];
     userService.getUsers().then(function(users) {
-        // TODO User will be selcting existing/new check box to selct/enter user name
-        // For now assuming there will be only one user per app.
         users.forEach(function(user) {
             console.log(user);
             var userForView = {'name':user.user_name, 'password':'', 'serverUrl':user.url};
             $scope.users.push(userForView);
         });
+        var last_visited_user = $scope.users[$scope.users.length-1];
+        $scope.current_user = last_visited_user;
         msg.hideAll();
        },function(error) {
         msg.hideLoadingWithErr('Unable to connect to local storage');
