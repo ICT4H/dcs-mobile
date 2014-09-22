7a5b4ee9aa021904a71e3773d54c0121ca6de724
Prince M Jain <connect2princejain@gmail.com>
2014-09-23 01:37:54 +0530

    Prince | Added alert after submitting submission.

--- dcs-app/www/js/controllers/submission-controller.js
+++ dcs-app/www/js/controllers/submission-controller.js
@@ -11,22 +11,58 @@ dcsApp.controller('submissionController', ['$routeParams', '$location', 'submiss
         return date.concat(' '+time)
     };
 
-    var redirect = function(path){
-        $location.path(path);
-    };
-
     var getSubmissionFromServer = function(submissionId) {
         return dcsService.getSubmission({'project_uuid':$routeParams.project_uuid, 'submission_uuid': submissionId});
     };
 
+    var onSubmit = function(submission, callBack) {
+        if (submission_id == 'null') {
+            onNew(submission, callBack);
+        } else {
+            onEdit(submission);
+        }
+    };
+
+    var onEdit = function(submission) {
+        localStore.updateSubmission(submission_id, submission)
+        .then(function() {
+            msg.displaySuccess('Updated');
+            $location.path("/submission-list/" + $routeParams.project_uuid);
+        }, function(error) {
+            console.log(error);
+        });
+    };
+
+    var onNew = function(submission, initializeForm) {
+        localStore.createSubmission(submission)
+        .then(function(submission) {
+            function onClick(buttonIndex) {
+                if(buttonIndex==BUTTON_NO) {
+                    msg.displaySuccess('Saved');
+                    $location.path("/submission-list/" + $routeParams.project_uuid);
+                }
+                else
+                    initializeForm();
+            };
+            navigator.notification.confirm(
+                'Do you want to submit another one?',
+                onClick,
+                'Delete submission',
+                ['Yes','No']
+            );
+            msg.displaySuccess('Saved');
+
+        }, function(error) {
+            console.log(error);
+        });
+    };
+
     options = {
-        'redirect': redirect,
+        'saveSubmission': onSubmit,
         'localStore': localStore,
         'buttonLabel': buttonLabel,
         'project_uuid': $routeParams.project_uuid,
         'submission_id': submission_id,
-        'onSuccess': msg.displaySuccess,
-        'onError': msg.displayError,
         'getDate': getDate,
         'getSubmissionFromServer': getSubmissionFromServer, 
         'isServerSubmission' : $routeParams.server ? true : false 
--- dcs-app/www/js/dcsApp.js
+++ dcs-app/www/js/dcsApp.js
@@ -7,7 +7,7 @@ var dcsApp = angular.module('dcsApp', ['ngRoute',
     "angucomplete-alt",
 ]);
 
-var isEmulator = false;
+var isEmulator = true;
 var BUTTON_NO = isEmulator ? 3 : 2;
 
 var SERVER = 'server';
