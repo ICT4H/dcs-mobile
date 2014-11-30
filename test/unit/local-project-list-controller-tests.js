5ef2710d9cf0480a04269bbe0e5c1590bf84a6cc
Yogesh Sreedharan <yogeshsr@thoughtworks.com>
2014-11-30 17:32:23 +0530

    Yogesh || Pushed the missted test spec file

---
+++ test/unit/local-project-list-controller-tests.js
@@ -0,0 +1,66 @@
+describe('local project list controller', function() {
+    var controller;
+    var rootScope;
+    var scope;
+    var locals;
+    var mocks;
+    jasmine.getJSONFixtures().fixturesPath = "base/i18n";
+    var data = loadJSONFixtures('resourceBundle.json');
+
+    beforeEach(angular.mock.module('dcsApp'));
+
+    beforeEach(angular.mock.inject(function($q, $httpBackend, $rootScope, $controller) {
+        this.$q = $q;
+        stubResourceBundleCall($httpBackend);        
+        initScope($rootScope);
+        controller = $controller;
+        mocks = new DCSMocks($q);
+    }));
+    
+    function stubResourceBundleCall($httpBackend) {
+        emptyRourceBundle = {};
+        $httpBackend.when("GET", 'i18n/resourceBundle.json').respond(emptyRourceBundle);        
+    }
+
+    function initScope($rootScope) {
+        rootScope = $rootScope.$new();
+        scope = rootScope.$new();
+        rootScope.pageSizes = [];
+        rootScope.pageSize = {value: 10};
+        rootScope.resourceBundle = data['resourceBundle.json'];
+    }
+
+    it('should list all local projects', function() {
+        mocks.add_project_dao_spy_1_local_project();
+        mocks.add_message_service_spy();
+        locals = {
+            $scope: scope,
+            $rootScope: rootScope,
+            projectDao: mocks.project_dao_with_1_local_project
+        };
+        controller('localProjectListController', locals);
+        scope.$apply();
+
+        expect(scope.projects.length).toEqual(1);
+    });
+
+    it('should update local outdated project status and load local project', function() {
+        mocks.add_project_dao_spy_1_local_project();
+        mocks.add_message_service_spy();
+        mocks.add_dcsService_spy_1_updated_server_project();
+        locals = {
+            $scope: scope,
+            $rootScope: rootScope,
+            projectDao: mocks.project_dao_with_1_local_project,
+            dcsService: mocks.dcsService_with_1_updated_server_project,
+            messageService: mocks.messageService
+        };
+        controller('localProjectListController', locals);
+        
+        scope.$sync();
+
+        scope.$apply();        
+        expect(mocks.project_dao_with_1_local_project.setprojectStatus).toHaveBeenCalledWith('1', 'OUTDATED');
+        expect(mocks.project_dao_with_1_local_project.getProjects).toHaveBeenCalled();
+    });
+});
